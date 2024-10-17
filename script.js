ede485b43d69b4b0b665ff40ca81fa8101a6c52c
Bruno Ribeiro <brunoribeiro.cgms@gmail.com>
2024-10-16 21:36:26 -0400

    editando arquivos html, css, script

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function mensagem() {
-  alert("produto cadastrado")
+
+
+function limparForm() {
+  document.querySelector("input[name= 'nome']").value = "" //limpar campos com esses id
+  document.querySelector("input[name= 'descricao']").value = ""
+  document.querySelector("input[name= 'preco']").value = ""
+
 }
+
+function adicionarProduto() {
+  console.log('adicionarProdutos')
+  const nome = document.querySelector('input[name="nome').value
+  const descricao = document.querySelector('input[name="descricao').value
+  const preco = document.querySelector('input[name="preco').value
+
+  console.log(`nome: ${nome}, descrição: ${descricao}, preço: ${preco}`)
+
+  const produtoNovo = `<p>nome: ${nome}</p> <p>descrição: ${descricao}</p> <p>preço: ${preco}</p>`
+  const elementoLista = document.querySelector('#produtos')
+
+  elementoLista.innerHTML = `${elementoLista.innerHTML} <li>${produtoNovo}</li>`
+
+  limparForm()
+
+
+
+}
+
